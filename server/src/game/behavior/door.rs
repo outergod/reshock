9e05909cb31cd87a4bf3c93bd3ed66763af4d001
Alexander Dorn <ad@not.one>
2022-08-25 16:12:06 +0200

    Prevent closing blocked doors

--- server/src/game/behavior/door.rs
+++ server/src/game/behavior/door.rs
@@ -1,13 +1,34 @@
 use bevy_ecs::prelude::*;
 
-use crate::game::*;
+use crate::game::{component::*, *};
 
-pub fn behavior(action: Res<ActiveAction>, mut followups: ResMut<FollowUps>) -> Status {
+pub fn behavior(
+    action: Res<ActiveAction>,
+    mut followups: ResMut<FollowUps>,
+    blockers: Query<&Position, With<Solid>>,
+    doors: Query<&Position, With<Door>>,
+) -> Status {
     match &action.0 {
-        Some(Action::OpenDoor(OpenDoorAction { actor, .. }))
-        | Some(Action::CloseDoor(CloseDoorAction { actor, .. })) => {
+        Some(Action::OpenDoor(OpenDoorAction { actor, .. })) => {
             followups.0.push(Action::EndTurn(*actor));
             Status::Accept
+        }
+        Some(Action::CloseDoor(CloseDoorAction { actor, entity })) => {
+            let position = match doors.get(*entity) {
+                Ok(it) => it,
+                Err(_) => {
+                    log::warn!("Tried to close door {:?} without Position", entity);
+                    return Status::Reject(None);
+                }
+            };
+
+            if blockers.iter().any(|pos| pos == position) {
+                let action = Action::Log("Door is blocked, can't close it".to_string());
+                Status::Reject(Some(action))
+            } else {
+                followups.0.push(Action::EndTurn(*actor));
+                Status::Accept
+            }
         }
         _ => Status::Continue,
     }
